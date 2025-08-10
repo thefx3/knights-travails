76b4a2dd2c4e57c4156370cd97cc8d0e3244fe6d
thefx3 <fx.pairault@yahoo.fr>
2025-08-10 19:45:32 +0200

    Tests

---
+++ main.js
@@ -0,0 +1,42 @@
+const moves = [
+    [2, 1], [2, -1], [-2, 1], [-2, -1],
+    [1, 2], [1, -2], [-1, 2], [-1, -2] ];
+
+
+function getKnightMoves([x, y]) {
+    return moves
+        .map(([dx, dy]) => [x+ dx, y+dy])
+        .filter(isValidPosition);
+}
+
+function knightTravails (start, end) {
+
+    let queue = [[start]];
+    const visited = new Set();
+    visited.add(start.toString());
+
+    while (queue.length > 0) {
+        const path = queue.shift();
+        const current = path[path.length - 1];
+
+        if (current.toString() === end.toString()) {
+            return path;
+        }
+
+        for (const move of getKnightMoves(current)) {
+            if (!visited.has(move.toString())) {
+                visited.add(move.toString());
+                queue.push([...path, move]);
+            }
+        }
+    }
+}
+
+//Check if the move is within the board
+function isValidPosition(array){
+    if ((array[0] >= 0 && array[0] <= 7) &&
+    (array[1] >= 0 && array[1] <= 7)) {
+        return true;
+    }
+    return false;
+}
